dfe455cf6ebba485b1364d173d7ac80a98f25b04
Tony Kamal Eldabaa <tonykamal122@gmail.com>
2025-07-11 23:54:14 +0300

    6- the user to enter a string and print the REVERSE of it

--- Assignment04/Program.cs
+++ Assignment04/Program.cs
@@ -81,13 +81,13 @@ class Program
 
         #region 6- the user to enter a string and print the REVERSE of it
 
-        /*
+        
         Console.WriteLine("Please Enter A word");
         string word = Console.ReadLine();
         StringBuilder word2 = new StringBuilder();
         for (int i = word.Length - 1; i >= 0; i--) word2.Append(word[i]);
         Console.WriteLine($"Reversed word:  {word2}");
-        */
+        
 
         #endregion
 
